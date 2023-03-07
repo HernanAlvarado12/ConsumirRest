5e72cd347ef8338b2f3b6dcfaa4a4c770b26e579
unknown <hernanvid123@gmail.com>
2023-03-06 22:22:26 -0500

    send model.js

--- model.js
+++ model.js
@@ -1,33 +1,76 @@
 'use strict'
 
-const URLRooter = 'https://api.thecatapi.com/v1/images/search?api_key=live_rltugyOJ3NfZE8VnfH0SDUGWPJMFa65AWqYpySVRVVK9YufsGVqR9V8WZYVXNyfL&limit=4'
-const querisParameters = ['limit=4']
+const URLRooter = 'https://api.thecatapi.com/v1'
+const API_KEY = 'api_key=live_rltugyOJ3NfZE8VnfH0SDUGWPJMFa65AWqYpySVRVVK9YufsGVqR9V8WZYVXNyfL'
+
+
+const typeRequest = {
+    DEFAULT: `${URLRooter}/images/search?${API_KEY}&limit=4`,
+    FAVORITES: `${URLRooter}/favourites?${API_KEY}`,
+}
 
 
 document.addEventListener('click', event => {
-    if(event.target.matches('#refresh')) {
+    /**
+     * @type {Element}
+     */
+    const target = event.target
+    if(target.matches('#refresh')) {
         loadImage()
+    }else if(target.matches('button#addFavorite')) {
+        const previousElement = target.previousElementSibling
+        saveFavoriteImage()
+        console.log('fin')
     }
 })
+document.addEventListener('DOMContentLoaded', loadImage)
 
 
+/**
+ * @returns {void}
+ */
 function loadImage() {
     const parentNode = document.getElementById('sectionImage')
     const documentFragment = document.createDocumentFragment()
     const imageTemplate = document.getElementById('imageTemplate').content
     cleanNodes(parentNode)
-    fetch(URLRooter)
+    fetch(typeRequest.DEFAULT)
         .then(response => response.json())
-        .then(json => {
-            for(let i = 0; i < 4; i++) {
-                const cloneNode = document.importNode(imageTemplate, true)
-                cloneNode.querySelector('img').setAttribute('src', json[i].url)
-                documentFragment.append(cloneNode)
-            }
-            parentNode.append(documentFragment)
-        }).catch(error => console.error(`Ha ocurrido un error de tipo ${error}`))
+        .then(json => loadImageTemplate(json, { templateContent: imageTemplate, documentFragment, parentInsert: parentNode }))
+        .catch(error => console.log(`Ha ocurrido un error de tipo ${error}`))
 }
-loadImage()
+
+
+/**
+ * @returns {void}
+ */
+function loadFavoriteImages() {
+    fetch(typeRequest.FAVORITES)
+        .then(response => console.log(response))
+}
+loadFavoriteImages()
+
+
+/**
+ * @param {}
+ * @returns {void}
+ */
+function saveFavoriteImage(idImage) {
+    const body = JSON.stringify({
+        'image_id': 'dje'
+    })
+    fetch(typeRequest.FAVORITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            image_id: 'e4b'
+        })
+    })
+    .then(json => console.log(json))
+    .catch(error => console.log(error))
+}
 
 
 /**
@@ -40,4 +83,24 @@ function cleanNodes(parentNode) {
             element.remove()
         }
     })
+}
+
+
+/**
+ * 
+ * @param {Array<Object>} arrayJson 
+ * @param {appendItemsTemplate} appendItemsTemplate
+ * @typedef {Object} appendItemsTemplate
+ * @property {Element} imageTemplate
+ * @property {DocumentFragment} documentFragment
+ * @property {Element} parentInsert
+ */
+function loadImageTemplate(arrayJson, { templateContent = '', documentFragment = '', parentInsert = '' }) {
+    arrayJson.forEach(element => {
+        //console.log(element)
+        const cloneNode = document.importNode(templateContent, true)
+        cloneNode.querySelector('img').setAttribute('src', element.url)  
+        documentFragment.append(cloneNode)
+    })
+    parentInsert.appendChild(documentFragment);
 }
